1fbd865961b6adbc9ecf36a28d56392482cee1a4
Sebil Alagoz <s.alagoz@outlook.fr>
2024-04-24 16:03:10 +0200

    problème API se déconnecte

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,11 +1,18 @@
 // Récupérer élements
 const form = document.querySelector(".login form");
 const errorMsg = document.querySelector(".login p");
-const email = document.querySelector(".login #email");
-const password = document.querySelector(".login #password");
 
-const response = await fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({ email, password })
-});
+
+
+
+form.addEventListener("submit", async function(event){
+        event.preventDefault()
+        const email = document.querySelector(".login #email");
+        const password = document.querySelector(".login #password");
+        const response = await fetch("http://localhost:5678/api/users/login", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({ "email": email, "password": password })
+        });
+console.log(response)
+})
