622e42ca0775aa8570330965f7245a82f54dc623
Sebil Alagoz <s.alagoz@outlook.fr>
2024-05-01 00:21:20 +0200

    Ajout d'une classe modal-form dans html et création du formulaire dynamiquement depuis le fichier main.js

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -106,11 +106,12 @@ if (login) {
         event.preventDefault()
         window.localStorage.removeItem("token");
         document.location.reload() //Revenir à la page d'accueil après logout
+        
     });
 } 
 
 
-//AFFICHAGE MODALE AU CLIC DU BOUTON MODIFIER
+//AFFICHAGE MODALE
 
 const modalContainer = document.querySelector(".modal-container")
 const modalWindow = document.querySelector(".modal-window")
@@ -118,17 +119,18 @@ const modalWindowContain = document.querySelector(".modal-window-contain")
 const closeBtn = document.querySelector(".fa-xmark")
 const arrowLeft = document.querySelector(".fa-arrow-left")
 const galleryModal = document.querySelector(".gallery-modal")
-
+const modalForm = document.querySelector(".modal-form")
 
 
 iconeDiv.addEventListener("click", function() {
     console.log("Modale ouverte")
     modalContainer.style.display = "flex";
+    modalWindowContain.innerHTML = ""
     //Créer .modal-window-contain
     const modalTitle = document.createElement("h3");
     modalTitle.textContent = "Galerie photo";
-    const galleryModal = document.createElement("div");
-    galleryModal.classList.add("gallery-modal")
+    // const galleryModal = document.createElement("div");
+    // galleryModal.classList.add("gallery-modal")
     const modalBtnAddImg = document.createElement("button");
     modalBtnAddImg.textContent = "Ajouter une photo"
     modalBtnAddImg.classList.add("btn-add-img")
@@ -137,13 +139,104 @@ iconeDiv.addEventListener("click", function() {
     modalWindowContain.appendChild(galleryModal);
     modalWindowContain.appendChild(modalBtnAddImg);
 
-    ajouterGalerie(projets);
+    // Création formulaire
+    modalBtnAddImg.addEventListener("click", function(){
+        console.log("Valider la photo")
+        arrowLeft.style.display = "flex";
+        galleryModal.innerHTML = ""
+        modalTitle.textContent = "Ajout photo"
+        modalBtnAddImg.textContent = "Valider"
+        modalBtnAddImg.classList.add("grey-btn")
+
+        const form = document.createElement("form")
+        form.classList.add("form-contain")
+        form.action = "#"
+        form.method = "post"
+
+        const addImgDiv = document.createElement("div")
+        addImgDiv.classList.add("add-img")
+
+        const iconeImg = document.createElement("i")
+        iconeImg.classList.add("fa-regular", "fa-image")
+
+        const imageLabel = document.createElement("label")
+        imageLabel.setAttribute("for", "file")
+        imageLabel.textContent = "+ Ajouter photo"
+
+        const imageInput = document.createElement("input")
+        imageInput.type = "file";
+        imageInput.id = "file";
+        imageInput.name = "image";
+
+        const imagePreview = document.createElement("img")
+        imagePreview.src = "#";
+        imagePreview.alt = "Aperçu de l'image";
+
+        const imageInfo = document.createElement("p")
+        imageInfo.textContent = "jpg, png : 4mo max"
+
+        addImgDiv.appendChild(iconeImg)
+        addImgDiv.appendChild(imageLabel)
+        addImgDiv.appendChild(imageInput)
+        addImgDiv.appendChild(imagePreview)
+        addImgDiv.appendChild(imageInfo)
+
+        const titleLabel = document.createElement("label")
+        titleLabel.setAttribute("for", "title")
+        titleLabel.textContent = "Titre"
+
+        const titleInput = document.createElement("input")
+        titleInput.type = "text"
+        titleInput.name = "title"
+        titleInput.id = "title"
+
+        const categoryLabel = document.createElement("label")
+        categoryLabel.setAttribute("for", "category")
+        categoryLabel.textContent = "Catégorie"
+
+        const categorySelect = document.createElement("select")
+        categorySelect.name = "category"
+        categorySelect.id = "category"
+
+        form.appendChild(addImgDiv)
+        form.appendChild(titleLabel)
+        form.appendChild(titleInput)
+        form.appendChild(categoryLabel)
+        form.appendChild(categorySelect)
+
+        modalForm.appendChild(form)
+    })
 })
 
 closeBtn.addEventListener("click", function(){
     console.log("Modale fermé")
     modalContainer.style.display = "none"
+    document.location.reload()
 })
 
 
+
 // Ajouter galerie dans modale
+async function displayGalleryModal (projets) {
+    modalWindowContain.innerHTML = ""
+    for(let i = 0; i < projets.length; i++){
+        const article = projets[i] 
+        const figure = document.createElement("figure")
+        const image = document.createElement("img")
+        image.src = article.imageUrl
+        image.alt = article.title
+        const trash = document.createElement("i")
+        trash.classList.add("fa-solid", "fa-trash-can")
+
+        figure.appendChild(trash)
+        figure.appendChild(image)
+        galleryModal.appendChild(figure)
+    }
+}
+displayGalleryModal(projets)
+
+
+
+
+// Supprimer image galerie dans modale
+
